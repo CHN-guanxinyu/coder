b9c42a9244127e30c445037ac145886c87144f12
paul <paul@outlook.com>
2018-05-21 12:21:09 +0800

    Version 1.2.0

--- deepScala/src/main/scala/com/speful/deepscala/algrithm/AStar.scala
+++ deepScala/src/main/scala/com/speful/deepscala/algrithm/AStar.scala
@@ -1,39 +1,29 @@
 package com.speful.deepscala.algrithm
 
-import scala.collection.mutable.{
-  Map => MMap,
-  ListBuffer
-}
-case class Point( x : Int = -1 , y : Int = -1 , var parent : Option[Point] = None){
-  def === (o : Point) = o.x == x && o.y == y
+import scala.annotation.tailrec
+import scala.collection.mutable.{ListBuffer, Map => MMap , Set => MSet }
 
+case class Point( x : Int = -1 , y : Int = -1 , var parent : Option[Point] = None){
   def moveTo( dxy : (Int, Int) ) = Point( dxy._1 + x , dxy._2 + y , parent )
 
   override def toString: String = s"( $x , $y )"
+
+  override def hashCode(): Int = (x,y).hashCode
+
+  override def equals(obj: Any): Boolean = hashCode == obj.hashCode
+
 }
+
 
 case class AStar[ T ] (val geo : Array[ Array[T] ] , wall : T , path : T , val step : Int = 10 ) {
 
 
-  def findPathAndShow ( start : Point , target : Point ){
 
-    val len = findPath(start, target).map{ case (x,y) => geo(x)(y) = path }.length
-
-    println(
-      s"""
-         |from : $start
-         |to : $target
-         |shortest lenth : $len
-      """.stripMargin)
-
-    geo.transpose map(_ mkString("  ")) map println
-
-  }
 
   //每个点的G的映射和H的映射
   lazy val GMap,HMap  = MMap[Point , Int]()
 
-  lazy val openList , closeList = ListBuffer[Point]()
+  lazy val openList , closeList = MSet[Point]()
 
   /**
     * 计算当前节点的G值
@@ -66,18 +56,37 @@ case class AStar[ T ] (val geo : Array[ Array[T] ] , wall : T , path : T , val s
   def F( p : Point ) = GMap.getOrElse (p , 0) + HMap.getOrElse(p , 0)
 
 
+  def findPathAndShow ( start : Point , target : Point ){
 
+    val len = findPath(start, target).map{ case (x,y) => geo(x)(y) = path }.length
 
+    println(
+      s"""
+         |from : $start
+         |to : $target
+         |shortest lenth : ${if(len == 0) "No path" else len}
+      """.stripMargin)
+
+    geo.transpose map(_ mkString("  ")) map println
+
+  }
+  /**
+    * AStar主算法
+    * @param start
+    * @param target
+    * @return
+    */
   def findPath( start : Point , target : Point ): List[(Int , Int)]= {
 
+    //初始化openList
     openList += start
 
-    var flag = true
+    //设置openList终止标记,当openList中存在target时终止
+    var stop = false
 
-    while ( openList.nonEmpty && flag ){
+    while ( openList.nonEmpty && !stop ){
       //F最小的点
       val curp = openList minBy F
-
       openList -= curp
       closeList += curp
 
@@ -86,7 +95,7 @@ case class AStar[ T ] (val geo : Array[ Array[T] ] , wall : T , path : T , val s
 
       neighbors map{ case p @ Point(x , y , _) =>
 
-        if( openList.exists( _ === p ) ){
+        if( openList exists( _ == target ) ){
           val newG = G(p)
           GMap get p filter( _ > newG ) map{ _ =>
             p parent = Some( curp )
@@ -100,19 +109,21 @@ case class AStar[ T ] (val geo : Array[ Array[T] ] , wall : T , path : T , val s
         }
       }
 
-      flag = ! openList.exists( _ === target )
+      stop = openList.exists( _ == target )
 
     }
 
-    var res = openList.find( _ === target ) getOrElse Point()
-    val parsedRes = ListBuffer((res.x , res.y))
-    while ( res.parent.nonEmpty ){
-      res = res.parent.get
-      parsedRes += ((res.x , res.y))
+    val finalPoint = openList.find( _ == target )
+
+    @tailrec
+    def makePaths( point : Option[Point] , res : ListBuffer[(Int,Int)] = ListBuffer.empty ) : List[(Int,Int)] = point match {
+      case None => res.result
+      case Some( Point( x , y , parent) ) =>
+        res += ((x , y))
+        makePaths( parent , res )
     }
-    parsedRes.result
 
-
+    makePaths( finalPoint )
   }
 
   /**
@@ -124,29 +135,33 @@ case class AStar[ T ] (val geo : Array[ Array[T] ] , wall : T , path : T , val s
     0 :: li zip li tail
   }
 
-  def genNeighbors(p : Point) = fourDir map p.moveTo
+  def genNeighbors : Point => List[Point] = fourDir map _.moveTo
 
   def withinGeo(p : Point) = geo.indices.contains( p x ) && geo.transpose.indices.contains( p y )
 
   def isWall(p : Point) = geo(p x)(p y) == wall
 
-  def inClose(p : Point) = closeList.exists(_ === p)
+  def inClose : Point => Boolean = closeList contains _
 
 
 }
 
 object Demo extends App{
 
-  val data = Array.tabulate[Char](9,9)( ( _ , _ ) => 'O')
-
-  //设置墙
-  Seq((3,3),(4,3),(5,3),(6,3),(7,3),(7,4),(7,5),(7,6),(7,7),(5,6),(6,6)).
-    map{case (x,y) => data(x)(y) = 'X'}
-
+  val raw_geo = List.tabulate[Char](9,9)( ( _ , _ ) => 'O')
   val start = Point( 8 , 6 )
   val target = Point( 5 , 4 )
 
-  AStar( data , wall = 'X' , path = '-').
-    findPathAndShow(start, target)
+
+  val geo1 = raw_geo.toArray.map(_ toArray)
+  //设置墙
+  Seq((3,3),(4,3),(5,3),(6,3),(7,3),(7,4),(7,5),(7,6),(7,7),(5,6),(6,6)).map{case (x,y) => geo1(x)(y) = 'X'}
+
+  AStar( geo1 , wall = 'X' , path = '-').findPathAndShow(start, target)
+
+  val geo2 = raw_geo.toArray.map(_ toArray)
+  Seq((3,3),(4,3),(5,3),(6,3),(7,3),(7,4),(7,5),(7,6),(7,7),(5,6),(6,6),(4,6),(4,5),(4,4)).map{case (x,y) => geo2(x)(y) = 'X'}
+
+  AStar( geo2 , wall = 'X' , path = '-').findPathAndShow(start, target)
 
 }
